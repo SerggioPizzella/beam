c50460c105e3e69a0531177410ebb71b102d1cd8
serggio <serggiopizzella@gmail.com>
2022-12-14 19:16:16 +0100

    we can receive files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use std::fs;
+
 #[macro_use]
 extern crate rocket;
 
@@ -8,14 +10,26 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/")]
+#[post("/task/<task_name>", data = "<task_content>")]
+fn receive_task(task_name: String, task_content: String) {
+    match fs::write(&task_name, task_content) {
+        Ok(_) => {
+            println!("task received and saved: {}.", task_name);
+            return 
+        },
+        Err(err) => {
+            eprintln!("task failed to be saved: {}. : {}", task_name, err)
+        }
+    }
+}
+
+#[get("/alive")]
 fn heart_beat() -> &'static str {
     "alive"
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
-        .mount("/alive", routes![heart_beat])
+        .mount("/", routes![index, heart_beat, receive_task])
         .launch();
 }
